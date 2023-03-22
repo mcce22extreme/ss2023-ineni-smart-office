2c6f092d194c23da033866423deb4716f7467bf9
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-03-22 21:25:40 +0100

    Added profiles

---
+++ src/Mcce22.SmartOffice.Management/Profiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Mcce22.SmartOffice.Management.Entities;
+using Mcce22.SmartOffice.Management.Models;
+
+namespace Mcce22.SmartOffice.Management.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserModel>();
+
+            CreateMap<SaveUserModel, User>();
+        }
+    }
+}
---
+++ src/Mcce22.SmartOffice.Management/Profiles/WorkspaceProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Mcce22.SmartOffice.Management.Entities;
+using Mcce22.SmartOffice.Management.Models;
+
+namespace Mcce22.SmartOffice.Management.Profiles
+{
+    public class WorkspaceProfile : Profile
+    {
+        public WorkspaceProfile()
+        {
+            CreateMap<Workspace, WorkspaceModel>();
+
+            CreateMap<SaveWorkspaceModel, Workspace>();
+        }
+    }
+}
