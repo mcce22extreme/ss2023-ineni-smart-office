a8734d220e56ca5f5252405573ae3e21ae7ba5f9
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-04-05 21:18:49 +0200

    Added validators

---
+++ src/Mcce22.SmartOffice.Management/Validators/SaveBookingValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Mcce22.SmartOffice.Management.Models;
+
+namespace Mcce22.SmartOffice.Management.Validators
+{
+    public class SaveBookingValidator : AbstractValidator<SaveBookingModel>
+    {
+        public SaveBookingValidator()
+        {
+            RuleFor(x => x.StartDateTime).LessThan(x => x.EndDateTime);
+
+            RuleFor(x => x.UserId).GreaterThan(0);
+
+            RuleFor(x => x.WorkspaceId).GreaterThan(0);
+        }
+    }
+}
---
+++ src/Mcce22.SmartOffice.Management/Validators/SaveWorkspaceValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Mcce22.SmartOffice.Management.Models;
+
+namespace Mcce22.SmartOffice.Management.Validators
+{
+    public class SaveWorkspaceValidator : AbstractValidator<SaveWorkspaceModel>
+    {
+        public SaveWorkspaceValidator()
+        {
+            RuleFor(x => x.WorkspaceNumber).NotEmpty();
+        }
+    }
+}
