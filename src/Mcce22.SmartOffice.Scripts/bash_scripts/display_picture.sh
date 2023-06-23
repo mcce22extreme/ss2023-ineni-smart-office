bb52e286c23293e7238e82c7556090f32c9d4044
Andreas Gruber <andreas@gruber.info>
2023-06-23 22:42:56 +0200

    Display picture can now handle more than 1 picture

--- src/Mcce22.SmartOffice.Scripts/bash_scripts/display_picture.sh
+++ src/Mcce22.SmartOffice.Scripts/bash_scripts/display_picture.sh
@@ -9,9 +9,6 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Save the web link parameter
-picture_link=$1
-
 # Check if a previous picture is displayed
 if pgrep -x feh >/dev/null; then
     # Kill the previous feh process
@@ -21,12 +18,25 @@ if pgrep -x feh >/dev/null; then
     sleep 1
 fi
 
-# Download the new picture using wget
-temp_file="/tmp/fullscreen_picture.jpg"
-wget -q -O "$temp_file" "$picture_link"
+echo "Parameters $@"
+# Make a temporary directory to store images
+tmpdir=$(mktemp -d)
+
+echo "created temp dir $tmpdir"
+
+# Loop over all arguments
+for url in "$@"; do
+    # Generate a random filename
+    filename=$(mktemp -p "$tmpdir")
+
+    # Download the image
+    curl -s "$url" -o "$filename"
+done
+
+ls -la $tmpdir
 
 # Display the new picture in fullscreen with a transition effect using feh
-feh --auto-zoom --fullscreen --quiet --hide-pointer --image-bg black --zoom fill --no-menus --slideshow-delay 5 --no-fehbg "$temp_file"
+feh --auto-zoom --fullscreen --quiet --hide-pointer --image-bg black --zoom fill --no-menus --slideshow-delay 5 --no-fehbg "$tmpdir"/*
 
-# Delete the temporary file
-rm "$temp_file"
+# Remove the temporary directory
+rm -r "$tmpdir"
