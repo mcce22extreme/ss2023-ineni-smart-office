8a30eb6e8532f60ef00dab7545e8f783262d738c
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-06-25 21:07:20 +0200

    Add workspace selection to simulator

---
+++ src/Mcce22.SmartOffice.Simulator/Models/WorkspaceModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mcce22.SmartOffice.Simulator
+{
+    public partial class WorkspaceModel : ObservableObject
+    {
+        public string Id { get; set; }
+
+        public string WorkspaceNumber { get; set; }
+    }
+}
